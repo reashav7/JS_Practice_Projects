ce691f5b672d0f536fe64d32c2eabbbb32dadd11
Reashav De <150650025+reashav7@users.noreply.github.com>
2024-07-15 20:53:05 +0530

    Initial commit

---
+++ 3-DigitalClock/project3.js
@@ -0,0 +1,13 @@
+const clock = document.getElementById('clock')
+// const clock = document.querySelector('#clock')
+
+
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000) 
+
+// write this syntax in this way only
+
+
